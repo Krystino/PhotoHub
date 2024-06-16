7813e3e19e8f6f2d652625cb71c40495dc867c4f
CC <faaa@live.com>
2024-06-16 20:38:17 +0800

    add love icon

--- src/app/gallery/image.tsx
+++ src/app/gallery/image.tsx
@@ -1,16 +1,21 @@
 "use client";
 
 import { CldImage } from 'next-cloudinary';
+import { OvalLoveTwo } from '@icon-park/react'
 
 
 export default function CImage(props: any) {
   return (
-    <CldImage
-      width="300"
-      height="300"
-      sizes="100vw"
-      alt="Description of my image"
-      {...props}
-    />
+    <div className='relative'>
+      <CldImage
+        width="300"
+        height="300"
+        sizes="100vw"
+        alt="Description of my image"
+        {...props}
+      />
+      <OvalLoveTwo className='absolute top-2 right-2' fill="#FFFFFF"  />
+    </div>
+
   )
 }
